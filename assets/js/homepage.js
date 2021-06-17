4124b84ea1f1130d8a43d8a307e4f9d19598b1cd
Brandon Evans <brandon.evans0708@gmail.com>
2021-06-16 20:54:40 -0600

    added new buttons to look for repos based on language and popularity

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons")
 
 var formSubmitHandler = function (event) {
    // prevent page from refreshing
@@ -39,7 +40,7 @@ var getUserRepos = function (user) {
       })
       .catch(function (error) {
          alert("Unable to connect to GitHub");
-   });
+      });
 };
 
 var displayRepos = function (repos, searchTerm) {
@@ -88,5 +89,33 @@ var displayRepos = function (repos, searchTerm) {
    }
 };
 
+var getFeaturedRepos = function (language) {
+   var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+   fetch(apiUrl).then(function (response) {
+      if (response.ok) {
+         response.json().then(function (data) {
+            displayRepos(data.items, language);
+         });
+      } else {
+         alert('Error: GitHub User Not Found');
+      }
+   });
+};
+
+
+
 // add event listeners to forms
 userFormEl.addEventListener("submit", formSubmitHandler);
+
+var buttonClickHandler = function (event) {
+   var language = event.target.getAttribute("data-language")
+   if (language) {
+      getFeaturedRepos(language);
+
+      // clear old content
+      repoContainerEl.textContent = "";
+   }
+}
+
+languageButtonsEl.addEventListener("click", buttonClickHandler);
